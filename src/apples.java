a671d83b299afad4e887dc30c114dceaa11bdcc0
Andrew <menning23@gmail.com>
2014-12-12 19:21:38 -0500

    Initial commit

---
+++ src/apples.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class apples {
+	public static void main(String args[]){
+		Scanner andrew = new Scanner(System.in);
+		
+		int girls, boys, people;
+		girls = 11;
+		boys = 3;
+		people = girls % boys;
+		System.out.println(people);
+	}
+}
