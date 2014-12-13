bc27d635cb82651da6c795f233ee157c62fe30b8
Andrew <menning23@gmail.com>
2014-12-12 22:26:53 -0500

    MultipleClasses

--- src/apples.java
+++ src/apples.java
@@ -1,24 +1,9 @@
 
-public class apples {
+class apples {
 	public static void main(String args[]){
-		int age;
-		age = 8;
 		
-		switch (age){
-		case 1:
-			System.out.println("You can crawl");
-			break;
-		case 2:
-			System.out.println("You can talk");
-			break;
-		case 3:
-			System.out.println("You can get in trouble");
-			break;
-		default:
-			System.out.println("I don't know how old you are");	
-			break;
-			
-		}
+		tuna tunaObject = new tuna();
+		tunaObject.simpleMessage();
 		
 	}
 }
