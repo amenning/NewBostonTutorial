2ec5ae4e82d6737d951a05c6865347b6b2a3d821
Andrew <menning23@gmail.com>
2014-12-15 20:06:06 -0500

    Revert "MultipleClasses"
    This reverts commit bc27d635cb82651da6c795f233ee157c62fe30b8.

--- src/apples.java
+++ src/apples.java
@@ -1,9 +1,24 @@
 
-class apples {
+public class apples {
 	public static void main(String args[]){
+		int age;
+		age = 8;
 		
-		tuna tunaObject = new tuna();
-		tunaObject.simpleMessage();
+		switch (age){
+		case 1:
+			System.out.println("You can crawl");
+			break;
+		case 2:
+			System.out.println("You can talk");
+			break;
+		case 3:
+			System.out.println("You can get in trouble");
+			break;
+		default:
+			System.out.println("I don't know how old you are");	
+			break;
+			
+		}
 		
 	}
 }
--- src/tuna.java
+++
@@ -1,6 +0,0 @@
-
-public class tuna {
-	public void simpleMessage(){
-		System.out.println("This is another class");
-	}
-}
